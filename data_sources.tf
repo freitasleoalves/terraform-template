668a742a23dafbc25634aa64da579b3d162f0f79
freitasleoalves <73323522+freitasleoalves@users.noreply.github.com>
2023-03-12 21:02:08 -0300

    template novos projetos terraform

--- data_sources.tf
+++
@@ -1,5 +0,0 @@
-# Used as workaround for bug in azuread provider
-# https://github.com/hashicorp/terraform-provider-azuread/issues/624#issuecomment-941128484
-data "azuread_user" "user" {
-  user_principal_name = var.user_principal_name
-}
